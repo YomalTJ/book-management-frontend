3984e970f07d5aaa94045c694795557f941e356f
YomalTJ <yomalthushara2295266@gmail.com>
2025-04-04 22:33:05 +0530

    Update .gitignore to include .vercel and add desktop.ini for folder view state configuration; modify next.config.ts to use experimental settings for swcMinify

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const nextConfig = {
+  experimental: {
+    swcMinify: true,
+  },
 };
 
 export default nextConfig;
+
